4f4de96bd55b6fc66b619da8a3194c264233673d
Mike O'Neill <moneill@qocinnovations.com>
2021-04-07 18:45:14 +0000

    SalesOrder test

---
+++ tests/sales_order/models/sales_order_inherit.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class SaleOrder(models.Model):
+      _inherit = 'sale.order'
+
+      sale_description = fields.Char(string='Sale Description', required=False)
+
