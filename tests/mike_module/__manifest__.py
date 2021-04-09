25f0550afc92119f2af732cd61b7637ef9dbbb90
Mike O'Neill <moneill@qocinnovations.com>
2021-04-09 15:05:37 +0000

    Odoo Menu Creation

--- tests/mike_module/__manifest__.py
+++ tests/mike_module/__manifest__.py
@@ -23,6 +23,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/cannabis_varieties.xml,
     ],
     # only loaded in demonstration mode
     'demo': [
